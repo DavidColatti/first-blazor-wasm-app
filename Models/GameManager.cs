18aa3e35a24c953e59c618a8612bfda59d071c76
David Colatti <davidcolatti@gmail.com>
2021-01-15 08:59:35 -0500

    feat: implemented pipes

--- Models/GameManager.cs
+++ Models/GameManager.cs
@@ -1,21 +1,24 @@
 ﻿using System;
-using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace FlappyBirdDemo.Web.Models
 {
-    public class GameManager : INotifyPropertyChanged
+    public class GameManager
     {
         private readonly int _gravity = 2;
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandler MainLoopCompleted;
 
-        public BirdModel Bird { get; set; }
-        public bool IsRunning { get; set; } = false;
+        public BirdModel Bird { get; private set; }
+        public List<PipeModel> Pipes { get; private set; }
+        public bool IsRunning { get; private set; } = false;
 
         public GameManager()
         {
             Bird = new BirdModel();
+            Pipes = new List<PipeModel>();
         }
 
         public async void MainLoop()
@@ -23,15 +26,11 @@ namespace FlappyBirdDemo.Web.Models
             IsRunning = true;
             while (IsRunning)
             {
-                Bird.Fall(_gravity);
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Bird)));
+                MoveObjects();
+                CheckForCollisions();
+                ManagePipes();
 
-                // if bird hit the ground then stop the game
-                if(Bird.DistanceFromGround <= 0)
-                {
-                    GameOver();
-                }
-
+                MainLoopCompleted?.Invoke(this, EventArgs.Empty);
                 await Task.Delay(20);
             }
         }
@@ -41,7 +40,47 @@ namespace FlappyBirdDemo.Web.Models
             if(!IsRunning)
             {
                 Bird = new BirdModel();
+                Pipes = new List<PipeModel>();
                 MainLoop();
+            }
+        }
+
+        public void Jump()
+        {
+            if (IsRunning)
+            {
+                Bird.Jump();
+            }
+        }
+
+        void MoveObjects()
+        {
+            Bird.Fall(_gravity);
+
+            foreach (var pipe in Pipes)
+            {
+                pipe.Move();
+            }
+        }
+
+        void CheckForCollisions()
+        {
+            if (Bird.IsOnGround())
+            {
+                GameOver();
+            }
+        }
+
+        void ManagePipes()
+        {
+            if(!Pipes.Any() || Pipes.Last().DistanceFromLeft <= 250)
+            {
+                Pipes.Add(new PipeModel());
+            }
+
+            if (Pipes.First().IsOffScreen())
+            {
+                Pipes.Remove(Pipes.First());
             }
         }
 
